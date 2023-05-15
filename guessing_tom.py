023dea2408c22e04ab5bcc5ef7837f94928c9f6f
filippopence <pencevgc@gmail.com>
2023-05-16 01:03:22 +0200

    removed useless line

--- guessing_tom.py
+++ guessing_tom.py
@@ -10,8 +10,6 @@ from unidecode import unidecode
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
-
 client = commands.Bot(command_prefix = '!', intents = discord.Intents.all())
 with open("Cards.json", "r", encoding="utf8") as f:
     data = json.load(f)
